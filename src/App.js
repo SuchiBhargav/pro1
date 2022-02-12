46ca5a83a620e80fbbfab4af7a967dd3781390f5
Suchu <hiiamsuchi@gmail.com>
2022-02-13 00:06:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,38 +1,35 @@
-
 import React from 'react';
+import './App.css';
+import Card from './Card.js'
+function App(){
+    return(
+        <div className='App'>
+         <Card
+          title='cold'
+          imageUrl='./images/cold.jpg'
+          body='cold, fever'
 
+         />
+         <Card
+          title=''
+          imageUrl=''
+          body=''
 
-class AppStreamCam extends React.Component {
-  constructor(props) {
-    super(props);
-    this.streamCamVideo= this.streamCamVideo.bind(this)
-  }
-  streamCamVideo() {
-    var constraints = { audio: true, video: { width: 1280, height: 720 } };
-    navigator.mediaDevices
-      .getUserMedia(constraints)
-      .then(function(mediaStream) {
-        var video = document.querySelector("video");
+         />
+         <Card
+          title=''
+          imageUrl=''
+          body=''
 
-        video.srcObject = mediaStream;
-        video.onloadedmetadata = function(e) {
-          video.play();
-        };
-      })
-      .catch(function(err) {
-        console.log(err.name + ": " + err.message);
-      }); // always check for errors at the end.
-  }
-  render() {
-    return (
-      <div>
-        <div id="container">
-          <video autoPlay={true} id="videoElement" controls></video>
+         />
+         <Card
+          title=''
+          imageUrl=''
+          body=''
+
+         />
         </div>
-        <br/>
-        <button onClick={this.streamCamVideo}>Start streaming</button>
-      </div>
     );
-  }
 }
-export default AppStreamCam; 
+export default App;
+
